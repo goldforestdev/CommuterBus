018cc82dcd8dcedabc806516596347023a683605
Yoonho Aaron Kim <yoonho.kim@hp.com>
2019-09-30 16:40:42 +0900

    한글명을 위한 필드 추가

--- app/src/main/java/com/hppk/sw/hppkcommuterbus/data/model/BusLine.kt
+++ app/src/main/java/com/hppk/sw/hppkcommuterbus/data/model/BusLine.kt
@@ -7,12 +7,14 @@ import kotlinx.android.parcel.Parcelize
 data class BusLine(
     val id: String = "",
     val name: String = "",
+    val nameKr: String = "",
     val busStops: List<BusStop> = listOf()
 ) : Parcelable
 
 @Parcelize
 data class BusStop(
     val name: String = "",
+    val nameKr: String = "",
     val time: String = "",
     val lat: Double = 0.0,
     val lng: Double = 0.0
