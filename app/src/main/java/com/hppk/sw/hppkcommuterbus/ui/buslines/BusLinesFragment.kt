22c8d617242a4a5d29baf9997e297037d2d27480
Yoonho Aaron Kim <yoonho.kim@hp.com>
2019-09-30 18:16:31 +0900

    map에 버스 노선 표기

--- app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/buslines/BusLinesFragment.kt
+++ app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/buslines/BusLinesFragment.kt
@@ -9,7 +9,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.hppk.sw.hppkcommuterbus.R
-import com.hppk.sw.hppkcommuterbus.ui.buslines.details.LineDetailsActivity
+import com.hppk.sw.hppkcommuterbus.data.model.BusLine
+import com.hppk.sw.hppkcommuterbus.data.model.BusStop
+import com.hppk.sw.hppkcommuterbus.ui.details.BUS_LINE
+import com.hppk.sw.hppkcommuterbus.ui.details.LineDetailsActivity
 import kotlinx.android.synthetic.main.fragment_bus_lines.*
 
 class BusLinesFragment : Fragment() {
@@ -23,7 +26,20 @@ class BusLinesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         btnTemp.setOnClickListener {
-            startActivity(Intent(context, LineDetailsActivity::class.java))
+            val busLine = BusLine(
+                id = "Bus Line 6",
+                name = "Bus Line 6",
+                nameKr = "버스 노선 6",
+                busStops = listOf(
+                    BusStop("한솔아파트", "한솔아파트", "7:40", 37.236318, 127.025801),
+                    BusStop("권곡사거리", "권곡사거리", "7:43", 37.246661, 127.032351),
+                    BusStop("경기소방본부", "경기소방본부", "7:45", 37.254801, 127.036062),
+                    BusStop("경기대 후문사거리", "경기대 후문사거리", "7:55", 37.296800, 127.040927),
+                    BusStop("HP", "HP", "8:17", 37.394306, 127.110189)
+                )
+            )
+            startActivity(Intent(context, LineDetailsActivity::class.java)
+                .putExtra(BUS_LINE, busLine))
         }
     }
 
---
+++ app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/details/LineDetailsActivity.kt
@@ -0,0 +1,59 @@
+package com.hppk.sw.hppkcommuterbus.ui.details
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.hppk.sw.hppkcommuterbus.R
+import com.hppk.sw.hppkcommuterbus.data.model.BusLine
+import kotlinx.android.synthetic.main.activity_line_details.*
+import net.daum.mf.map.api.*
+
+
+const val BUS_LINE = "busline"
+
+class LineDetailsActivity : AppCompatActivity() {
+
+    private val mapView: MapView by lazy { MapView(this) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_line_details)
+
+        if (!intent.hasExtra(BUS_LINE)) {
+            finish()
+            return
+        }
+
+        val busLine = intent.getParcelableExtra<BusLine>(BUS_LINE)
+        showBusLine(busLine)
+        mapContainer.addView(mapView)
+    }
+
+    private fun showBusLine(busLine: BusLine) {
+        val polyline = MapPolyline()
+        polyline.tag = 1000
+        polyline.lineColor = resources.getColor(R.color.colorAccent)
+        polyline.addPoints(
+            busLine.busStops.map { busStop ->
+                MapPoint.mapPointWithGeoCoord(busStop.lat, busStop.lng)
+            }.toTypedArray()
+        )
+        mapView.addPolyline(polyline)
+
+        mapView.addPOIItems(
+            busLine.busStops.map { busStop ->
+                MapPOIItem().apply {
+                    itemName = busStop.name
+                    mapPoint = MapPoint.mapPointWithGeoCoord(busStop.lat, busStop.lng)
+                    markerType = MapPOIItem.MarkerType.BluePin
+                    selectedMarkerType = MapPOIItem.MarkerType.RedPin
+                }
+            }.toTypedArray()
+        )
+
+        val mapPointBounds = MapPointBounds(polyline.mapPoints)
+        val padding = 200
+        mapView.moveCamera(CameraUpdateFactory.newMapPointBounds(mapPointBounds, padding))
+    }
+
+}
