f80082130f3f5927624247c11853ab6a2ec1558b
Yoonho Aaron Kim <yoonho.kim@hp.com>
2019-10-07 14:09:59 +0900

    하드 코딩 값 상수로 변경

--- app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/current/CurrentContract.kt
+++ app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/current/CurrentContract.kt
@@ -3,6 +3,9 @@ package com.hppk.sw.hppkcommuterbus.ui.current
 import com.hppk.sw.hppkcommuterbus.data.model.BusLine
 import com.hppk.sw.hppkcommuterbus.data.model.BusStop
 
+
+private const val DEFAULT_DISTANCE = 500F // 500m
+
 interface CurrentContract {
     interface View {
         fun onCurrentLocationLoaded(lat: Double, lng: Double)
@@ -13,6 +16,6 @@ interface CurrentContract {
     interface Presenter {
         fun unsubscribe()
         fun getCurrentLocation()
-        fun getNearByBusStops(lat: Double, lng: Double, busLineData: MutableList<BusLine>, distance: Float = 500f)
+        fun getNearByBusStops(lat: Double, lng: Double, busLineData: MutableList<BusLine>, distance: Float = DEFAULT_DISTANCE)
     }
 }
