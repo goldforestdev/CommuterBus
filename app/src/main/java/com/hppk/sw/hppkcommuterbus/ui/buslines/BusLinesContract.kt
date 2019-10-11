5c48b4d1b1b0101b86329c98273cef077cfb35b4
Hyeongju-Kim <hyeongju.kim@hp.com>
2019-10-11 15:55:45 +0900

    Code Review 반영2

--- app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/buslines/BusLinesContract.kt
+++ app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/buslines/BusLinesContract.kt
@@ -1,11 +1,14 @@
 package com.hppk.sw.hppkcommuterbus.ui.buslines
 
+import android.content.SharedPreferences
+
 interface BusLinesContract {
 
     interface View {
-
+        fun onFavoritesListLoaded(favoritesList : MutableList<String>)
     }
 
     interface Presenter {
+        fun loadRecent(pref: SharedPreferences)
     }
 }
--- app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/buslines/BusLinesPresenter.kt
+++ app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/buslines/BusLinesPresenter.kt
@@ -1,5 +1,16 @@
 package com.hppk.sw.hppkcommuterbus.ui.buslines
 
-class BusLinesPresenter() : BusLinesContract.Presenter {
+import android.content.SharedPreferences
+import com.hppk.sw.hppkcommuterbus.data.local.FavoritesLocalDataSource
+
+class BusLinesPresenter (
+    private val view : BusLinesContract.View
+) : BusLinesContract.Presenter {
+    private lateinit var dataList :MutableList<String>
+
+    override fun loadRecent(pref:SharedPreferences) {
+        dataList = FavoritesLocalDataSource.loadFavoriteID(pref)
+        view.onFavoritesListLoaded(dataList)
+    }
 
 }
