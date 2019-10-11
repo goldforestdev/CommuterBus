b045db2e33277bb58260d5fbb820f535ea63beea
Yoonho Aaron Kim <yoonho.kim@hp.com>
2019-10-11 16:55:44 +0900

    퇴근 알람 기본 구현

---
+++ app/src/main/java/com/hppk/sw/hppkcommuterbus/receiver/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,30 @@
+package com.hppk.sw.hppkcommuterbus.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingEvent
+import com.hppk.sw.hppkcommuterbus.R
+import com.hppk.sw.hppkcommuterbus.manager.NotiManager
+
+
+class GeofenceBroadcastReceiver : BroadcastReceiver() {
+
+    private val TAG = GeofenceBroadcastReceiver::class.java.simpleName
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+        if (geofencingEvent.hasError()) {
+            Log.e(TAG, "[BUS] onReceive - error: ${geofencingEvent.errorCode}")
+            return
+        }
+
+        if (geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            val message = intent.getStringExtra(KEY_ALARM_MESSAGE)
+            NotiManager.notify(context, context.getString(R.string.bus_alarm), message)
+        }
+    }
+
+}
