2d043f1f5f66c1f8ecb10c6767e3ba35bf7ca410
Yoonho Aaron Kim <yoonho.kim@hp.com>
2019-10-22 13:22:33 +0900

    busline, busstop 이름 얻는 로직의 단일화

--- app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/buslines/BusLinesAdapter.kt
+++ app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/buslines/BusLinesAdapter.kt
@@ -8,20 +8,25 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.hppk.sw.hppkcommuterbus.R
-import com.hppk.sw.hppkcommuterbus.application.CommuterBusApplication
 import com.hppk.sw.hppkcommuterbus.data.model.BusLine
 import kotlinx.android.synthetic.main.item_bus_list.view.*
 
 class BusLinesAdapter(
-    val busLines : MutableList<BusLine> = mutableListOf(),
-    val favorites : MutableList<String> = mutableListOf(),
-    private var context : Context? = null,
-    private val busLineClickListener : BusLineClickListener,
-    private val busFavoritesClickListener : BusFavoritesClickLister
+    val busLines: MutableList<BusLine> = mutableListOf(),
+    val favorites: MutableList<String> = mutableListOf(),
+    private var context: Context? = null,
+    private val busLineClickListener: BusLineClickListener,
+    private val busFavoritesClickListener: BusFavoritesClickLister
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         context = parent.context
-        return BusLinesHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_bus_list, parent, false))
+        return BusLinesHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_bus_list,
+                parent,
+                false
+            )
+        )
     }
 
     interface BusLineClickListener {
@@ -32,41 +37,36 @@ class BusLinesAdapter(
         fun onBusFavoritesClick(busLine: BusLine)
     }
 
-    override fun getItemCount(): Int  = busLines.size
+    override fun getItemCount(): Int = busLines.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         if (holder is BusLinesHolder) {
-            with (holder) {
-                if (CommuterBusApplication.language != "ko") {
-                    tvBusLineName.text = busLines[position].name
-                    tvBusStart.text =
-                        "${busLines[position].busStops[0].name} ${context!!.resources.getString(R.string.start)}"
-                } else {
-                    tvBusLineName.text = busLines[position].nameKr
-                    tvBusStart.text =
-                        "${busLines[position].busStops[0].nameKr} ${context!!.resources.getString(R.string.start)}"
-                }
+            with(holder) {
+                val busLine = busLines[position]
+
+                tvBusLineName.text = busLine.busLineName
+                tvBusStart.text = "${busLine.busStops[0].busStopName} ${context!!.resources.getString(R.string.start)}"
                 itemView.setOnClickListener {
-                    busLineClickListener.onBusLineClick(busLines[position])
+                    busLineClickListener.onBusLineClick(busLine)
                 }
 
-                if (favorites.contains(busLines[position].id) ) {
+                if (favorites.contains(busLine.id)) {
                     ivStar.setImageResource(android.R.drawable.star_big_on)
                 } else {
                     ivStar.setImageResource(android.R.drawable.star_big_off)
                 }
 
                 ivStar.setOnClickListener {
-                    busFavoritesClickListener.onBusFavoritesClick(busLines[position])
+                    busFavoritesClickListener.onBusFavoritesClick(busLine)
                 }
             }
         }
     }
 
 
-    class BusLinesHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val tvBusLineName : TextView = itemView.tvBusLineName
-        val tvBusStart : TextView = itemView.tvBusStart
-        val ivStar : ImageView = itemView.ivStar
+    class BusLinesHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvBusLineName: TextView = itemView.tvBusLineName
+        val tvBusStart: TextView = itemView.tvBusStart
+        val ivStar: ImageView = itemView.ivStar
     }
 }
