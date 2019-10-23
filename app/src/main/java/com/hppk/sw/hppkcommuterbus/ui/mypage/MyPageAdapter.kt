4b742e7c73c655ef9b79e4ab9dd59363f9cbda3b
Hyeongju-Kim <hyeongju.kim@hp.com>
2019-10-23 11:19:12 +0900

    Bus stop & Bus line에 대한 My Page 구성

--- app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/mypage/MyPageAdapter.kt
+++ app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/mypage/MyPageAdapter.kt
@@ -17,38 +17,54 @@ import kotlinx.android.synthetic.main.item_title.view.*
 
 
 class MyPageAdapter(
-    private val list : MutableList<Any>,
-    private var context : Context? = null
+    val list : MutableList<Any>,
+    private var context : Context? = null,
+    private val busLineClickListener : BusLineClickLister,
+    private val busStopClickListener : BusStopClickListener,
+    private val busFavoritesClickLister: BusFavoritesClickLister,
+    private val busAlarmClickLister: BusAlarmClickLister
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     companion object {
         const val TITLE_CONTENT = 0
-        const val OFFICE_ALARM_CONTENT = 1
-        const val HOME_ALARM_CONTENT = 2
+        const val ALARM_CONTENT = 1
         const val FAVORITE = 3
+    }
+
+    interface BusStopClickListener {
+        fun onBusStopClick(busStop: BusStop)
+    }
+
+    interface BusLineClickLister {
+        fun onBusLineClick(busLine: BusLine)
+    }
+
+    interface BusFavoritesClickLister {
+        fun onFavoritesClick(index : Int, busLine : BusLine)
+    }
+
+    interface BusAlarmClickLister {
+        fun onAlarmClick(index : Int, busStop : BusStop)
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val view : View
+        val view: View
         context = parent.context
-        val inflater : LayoutInflater = parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val inflater: LayoutInflater =
+            parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
         when (viewType) {
             TITLE_CONTENT -> {
-                view = inflater.inflate(R.layout.item_title, parent,false)
+                view = inflater.inflate(R.layout.item_title, parent, false)
                 return TitleHolder(view)
             }
-            OFFICE_ALARM_CONTENT -> {
-                view = inflater.inflate(R.layout.item_my_page_bus_stop, parent,false)
-                return BusAlarmHolder(view)
-            }
-            HOME_ALARM_CONTENT -> {
-                view = inflater.inflate(R.layout.item_my_page_bus_stop, parent,false)
+            ALARM_CONTENT -> {
+                view = inflater.inflate(R.layout.item_my_page_bus_stop, parent, false)
                 return BusAlarmHolder(view)
             }
             FAVORITE -> {
-                view = inflater.inflate(R.layout.item_bus_list, parent,false)
+                view = inflater.inflate(R.layout.item_bus_list, parent, false)
                 return FavoriteHolder(view)
             }
             else -> {
@@ -56,42 +72,35 @@ class MyPageAdapter(
             }
         }
     }
-
-    interface BusLineClickListener {
-        fun onBusLineClick(busLine: BusLine)
-    }
-
-    interface BusFavoritesClickLister {
-        fun onBusFavoritesClick(busLine: BusLine)
-    }
-
     override fun getItemCount(): Int = list.size
 
     override fun getItemViewType(position: Int): Int {
         return when {
             list[position] is String -> TITLE_CONTENT
-            list[position] is BusStop -> HOME_ALARM_CONTENT
+            list[position] is BusStop -> ALARM_CONTENT
             list[position] is BusLine -> FAVORITE
             else -> super.getItemViewType(position)
         }
     }
 
 
-
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val obj = list[position]
 
         when (holder) {
-            is TitleHolder -> with (holder) {
+            is TitleHolder -> with(holder) {
                 tvTitle.text = (obj as String)
             }
-            is BusAlarmHolder -> with (holder) {
-                tvName.text = if (CommuterBusApplication.language != "ko") (obj as BusStop).name else (obj as BusStop).nameKr
+            is BusAlarmHolder -> with(holder) {
+                tvName.text =
+                    if (CommuterBusApplication.language != "ko") (obj as BusStop).name else (obj as BusStop).nameKr
 
                 tvTime.text = obj.time
                 ivAlarm.setImageResource(R.drawable.ic_alarm_selected)
+                ivAlarm.setOnClickListener { busAlarmClickLister.onAlarmClick(position,obj) }
+                itemView.setOnClickListener { busStopClickListener.onBusStopClick(obj) }
             }
-            is FavoriteHolder -> with (holder) {
+            is FavoriteHolder -> with(holder) {
                 if (CommuterBusApplication.language != "ko") {
                     tvBusLineName.text = (obj as BusLine).name
                     tvBusStart.text =
@@ -102,30 +111,30 @@ class MyPageAdapter(
                         "${obj.busStops[0].nameKr} ${context!!.resources.getString(R.string.start)}"
                 }
                 ivStar.setImageResource(android.R.drawable.star_big_on)
+                ivStar.setOnClickListener {
+                    busFavoritesClickLister.onFavoritesClick(position,obj)
+                }
+                itemView.setOnClickListener {
+                    busLineClickListener.onBusLineClick(obj)
+                }
             }
         }
     }
 
-    class TitleHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvTitle : TextView = itemView.tvTitle
+    inner class TitleHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvTitle: TextView = itemView.tvTitle
     }
 
-    class BusAlarmHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvName : TextView = itemView.tvName
-        val tvTime : TextView = itemView.tvTime
-        val ivAlarm : ImageView = itemView.ivAlarm
+    inner class BusAlarmHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvName: TextView = itemView.tvName
+        val tvTime: TextView = itemView.tvTime
+        val ivAlarm: ImageView = itemView.ivAlarm
     }
 
-    class FavoriteHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvBusLineName : TextView = itemView.tvBusLineName
-        val tvBusStart : TextView = itemView.tvBusStart
-        val ivStar : ImageView = itemView.ivStar
+    inner class FavoriteHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvBusLineName: TextView = itemView.tvBusLineName
+        val tvBusStart: TextView = itemView.tvBusStart
+        val ivStar: ImageView = itemView.ivStar
     }
 
-
-   /* class BusLinesHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val tvBusLineName : TextView = itemView.tvBusLineName
-        val tvBusStart : TextView = itemView.tvBusStart
-        val ivStar : ImageView = itemView.ivStar
-    }*/
 }
--- app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/hppk/sw/hppkcommuterbus/ui/mypage/MyPageFragment.kt
@@ -1,6 +1,7 @@
 package com.hppk.sw.hppkcommuterbus.ui.mypage
 
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
@@ -12,18 +13,27 @@ import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hppk.sw.hppkcommuterbus.R
+import com.hppk.sw.hppkcommuterbus.data.local.LocalDataSource
 import com.hppk.sw.hppkcommuterbus.data.model.BusLine
 import com.hppk.sw.hppkcommuterbus.data.model.BusStop
 import com.hppk.sw.hppkcommuterbus.data.model.Type
+import com.hppk.sw.hppkcommuterbus.ui.MainActivity
+import com.hppk.sw.hppkcommuterbus.ui.buslines.BusLinesAdapter
+import com.hppk.sw.hppkcommuterbus.ui.details.BUS_LINE
+import com.hppk.sw.hppkcommuterbus.ui.details.LineDetailsActivity
+import com.hppk.sw.hppkcommuterbus.ui.splash.SplashPresenter
 import kotlinx.android.synthetic.main.fragment_my_page.*
 
-class MyPageFragment : Fragment(), MyPageContract.View {
+class MyPageFragment : Fragment(), MyPageContract.View, MyPageAdapter.BusLineClickLister
+    , MyPageAdapter.BusStopClickListener, MyPageAdapter.BusFavoritesClickLister, MyPageAdapter.BusAlarmClickLister {
 
     private val pref: SharedPreferences by lazy { PreferenceManager.getDefaultSharedPreferences(activity) }
     private val presenter: MyPageContract.Presenter by lazy { MyPagePresenter(this) }
     private lateinit var myPageAdapter: MyPageAdapter
     private lateinit var favoritesBusLineList :MutableList<BusLine>
+    private lateinit var alarmBusLineList :MutableList<BusStop>
     private lateinit var busStopMap : Map<Type, List<BusStop>>
+    private val TAG = MyPageFragment::class.java.simpleName
 
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,12 +46,11 @@ class MyPageFragment : Fragment(), MyPageContract.View {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_page, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onResume() {
+        super.onResume()
 
         initData()
     }
@@ -52,13 +61,12 @@ class MyPageFragment : Fragment(), MyPageContract.View {
 
     override fun onDataLoaded(favoritesList : MutableList<BusLine>, alarmList : MutableList<BusStop>){
         favoritesBusLineList = favoritesList
-        busStopMap = alarmList.groupBy { it.type }
+        alarmBusLineList = alarmList
+        busStopMap = alarmBusLineList.groupBy { it.type }
         initRecyclerView()
     }
 
     private fun initRecyclerView(){
-
-
         val list = mutableListOf<Any>()
         try {
             if (busStopMap.getValue(Type.GO_OFFICE).isNotEmpty()) {
@@ -66,7 +74,7 @@ class MyPageFragment : Fragment(), MyPageContract.View {
                 list.addAll(busStopMap.getValue(Type.GO_OFFICE))
             }
         } catch (e : NoSuchElementException) {
-            Log.e("MyPageFragment",e.message)
+            Log.e(TAG,e.message)
         }
 
         try {
@@ -75,7 +83,7 @@ class MyPageFragment : Fragment(), MyPageContract.View {
                 list.addAll(busStopMap.getValue(Type.LEAVE_OFFICE))
             }
         } catch (e : NoSuchElementException) {
-            Log.e("MyPageFragment",e.message)
+            Log.e(TAG,e.message)
         }
 
 
@@ -84,15 +92,93 @@ class MyPageFragment : Fragment(), MyPageContract.View {
             list.addAll(favoritesBusLineList)
         }
 
-
-
-        myPageAdapter = MyPageAdapter(list = list)
+        myPageAdapter = MyPageAdapter(list = list, busLineClickListener = this
+            , busStopClickListener = this, busFavoritesClickLister = this, busAlarmClickLister = this)
         rcMyPageList.adapter = myPageAdapter
         rcMyPageList.layoutManager = LinearLayoutManager(activity)
-
 
         myPageAdapter.notifyDataSetChanged()
     }
 
+
+    override fun onBusStopClick(busStop: BusStop) {
+        var callBusLineData : BusLine? = null
+        activity?.apply {
+            if (this is MainActivity) {
+                val busLineDataList = this.busLineData
+                for (busLine in busLineDataList) {
+                    if (callBusLineData != null) {
+                        break
+                    }
+                    for (busStopData in busLine.busStops) {
+                        if (busStopData.index == busStop.index) {
+                            callBusLineData = busLine
+                            break
+                        }
+                    }
+                }
+            }
+        }
+
+        if(callBusLineData != null) {
+            startActivity(
+                Intent(context, LineDetailsActivity::class.java)
+                    .putExtra(BUS_LINE, callBusLineData)
+            )
+        }
+    }
+
+    override fun onBusLineClick(busLine: BusLine) {
+        startActivity(
+            Intent(context, LineDetailsActivity::class.java)
+                .putExtra(BUS_LINE, busLine)
+        )
+    }
+
+    override fun onFavoritesClick(index: Int, busLine: BusLine) {
+        if (favoritesBusLineList.contains(busLine)) {
+            favoritesBusLineList.remove(busLine)
+            myPageAdapter.list.removeAt(index)
+            if(favoritesBusLineList.size == 0) {
+                myPageAdapter.list.removeAt(index-1)
+            }
+            myPageAdapter.notifyDataSetChanged()
+            LocalDataSource.saveFavoriteID(pref,favoritesBusLineList)
+        }
+    }
+
+    override fun onAlarmClick(index: Int, busStop: BusStop) {
+        if (busStop.type == Type.GO_OFFICE) {
+            alarmListRemove(busStop, index)
+            try {
+                Log.i(TAG,getMapSize(Type.GO_OFFICE).toString())
+            } catch (e : NoSuchElementException) {
+                myPageAdapter.list.removeAt(index-1)
+            }
+
+        } else {
+            alarmListRemove(busStop, index)
+            try {
+                Log.i(TAG,getMapSize(Type.LEAVE_OFFICE).toString())
+            } catch (e : NoSuchElementException) {
+                myPageAdapter.list.removeAt(index-1)
+            }
+        }
+        myPageAdapter.notifyDataSetChanged()
+        LocalDataSource.saveAlarm(pref, alarmBusLineList)
+    }
+
+    private fun getMapSize(key : Type) : Int = busStopMap.getValue(key).size
+
+    private fun alarmListRemove(
+        busStop: BusStop,
+        index: Int
+    ) {
+        if (alarmBusLineList.contains(busStop)) {
+            alarmBusLineList.remove(busStop)
+            myPageAdapter.list.removeAt(index)
+            busStopMap = alarmBusLineList.groupBy { it.type }
+        }
+    }
 
 }
